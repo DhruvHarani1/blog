6f6336118bb46ccae1d3ef92850b968287006ca7
DhruvHarani1 <dhruvharani5@gmail.com>
2025-11-23 19:31:25 +0530

    initial setup of redx , store and basic app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import config from './config/config'
+import { useEffect, useState } from "react";
+import "./App.css";
+import config from "./config/config";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header } from "./components";
+import {Footer} from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-   console.log(config.appWriteUrl)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-    
-  
-    
-    </>
-  )
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <div className="bg-gray-600">helloooo</div>
+        <div>
+          <Header />
+          <main>{/*  <Outlet></Outlet>*/}</main>
+          <Footer />
+        </div>
+      </>
+    );
+  } else {
+    return null;
+  }
 }
 
-export default App
+export default App;
